7eaca3db0c5ebad1f37c70d19586586bac8d53e6
oraby <amrorapy50@gmail.com>
2026-05-22 07:28:18 +0300

    Create Start

---
+++ src/ui/Start.tsx
@@ -0,0 +1,14 @@
+import { useQuiz } from "../context/QuizContext";
+
+function Start() {
+    const { numQuestions, dispatch } = useQuiz();
+    return (
+        <div className="start">
+            <h2>Welcome to the react quiz</h2>
+            <h3>{numQuestions} questions to test your React mastery</h3>
+            <button className="btn btn-ui" onClick={() => dispatch({ type: "active" })}>Let's start</button>
+        </div>
+    );
+}
+
+export default Start;
