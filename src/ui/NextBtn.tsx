96af236854d6e956eccdb1d495c5223ef7cdceec
oraby <amrorapy50@gmail.com>
2026-05-22 07:25:51 +0300

    Create NextBtn

---
+++ src/ui/NextBtn.tsx
@@ -0,0 +1,21 @@
+import { useQuiz } from "../context/QuizContext";
+
+function NextBtn() {
+  const { answer, dispatch, index, numQuestions } = useQuiz();
+  return (
+    <>
+      {answer && (
+        <button
+          className="btn btn-ui"
+          onClick={() =>
+            dispatch({ type: index === numQuestions - 1 ? "finish" : "next" })
+          }
+        >
+          {index === numQuestions - 1 ? "Finsih" : "Next"}
+        </button>
+      )}
+    </>
+  );
+}
+
+export default NextBtn;
